a4ce7d92a52a12f8ad29d7a05565340d976cad8d
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-06-06 17:07:40 -0500

    Experiment with logic expressions

--- module_03_import_file_io.py
+++ module_03_import_file_io.py
@@ -4,8 +4,12 @@ Author: Amanda Dadalt Makino
 Date: June 02, 2025
 Usage: To execute, press the play button in the VSC IDE.
 """
-# IMPORT STATEMENTS
 
+# IMPORT STATEMENTS
+from math import sqrt
+import math
+import random
+from pprint import pprint
 
 # Variables used in this demonstration
 radius = 12.5
@@ -13,21 +17,35 @@ square = 144
 fruits = ["apple", "banana", "cherry"]
 
 # USING IMPORTED MODULES
-
-
+area = math.pi * radius ** 2
+root = sqrt(square)
+"""
+print(root)
+print (area)
+"""
+"""
 # RANDOM
 
-
+print(random.random())
+print(random.randint(1,6))
 
 
 # FILE I/O
 
+with open("example.txt", "r") as file:
+    content= file.read()
+    print(content)
 
-
-
+"""
 # WITH CLAUSE
 
-
+data = {}
+with open("dict_example.txt", "r") as bob:
+    for pawan in bob:
+        key, value = pawan.strip().split('*')
+        data[key] = int(value)
+print(data)
+pprint(data)
 
 
 # READ INTO DICTIONARY
--- module_03_loops.py
+++ module_03_loops.py
@@ -11,8 +11,15 @@ matrix = [[1, 2, 3],
           [4, 5, 6],
           [7, 8, 9]]
 
-# FOR LOOP
 
+# FOR LOOP
+for fruit in fruits: 
+    print(fruit)
+
+for i in range(-10, 0):
+    print(i)
+
+"""
 # INPUT FUNCTION
 
 # print(f"Name: {name} \nSalary: ${salary:,.2f}")
@@ -20,20 +27,29 @@ matrix = [[1, 2, 3],
 
 # WHILE LOOP
 
+favorite_number = 0
+while favorite_number < 100:
+    favorite_number = int(input("Enter your favorite number (but not 7), 100 & up to quit: "))
+    if favorite_number == 7:
+        print("You broke this game.")
+        #break
+else: 
+    print("Your favorite number is too big.")
 
 # LOOP CONTROL STATEMENTS
+
 # CONTINUE:
-"""
+
 for number in range(1, 10):
     # Use continue statement to skip over multiples of 3.
     if number % 3 == 0:
         continue
     # Print all other integers
     print(number)
-"""
+
 
 #BREAK:
-"""
+
 # Use a for loop to iterate over the range of integers 
 for number in range(1, 10):
     # Use the break statement to exit the loop if the integer is greater than 5
@@ -45,7 +61,17 @@ for number in range(1, 10):
 
 # INFINITE LOOP
 # <ctrl> + c (in the terminal window) to stop an infinite loop in VS Code.
-
+number = 10 
+while number > 0 :
+    if number > 100:
+        break
+    number += 1 
+    print(number)
 
 # NESTED LOOP
 # matrix variable defined at top of editor.
+
+for row in matrix:
+    for element in row:
+        print(element, end = '')
+        print()
